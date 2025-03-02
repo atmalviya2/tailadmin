4674008c84042c96d7d58ae32d0d06a4c5cfc03c
wayne <wayne.o.motive@gmail.com>
2025-03-02 08:21:34 +0800

    final version

--- src/components/OrderCountChart.tsx
+++ src/components/OrderCountChart.tsx
@@ -46,7 +46,7 @@ const options: ApexOptions = {
 
   xaxis: {
     categories: [
-      "Total Finished",
+      "Total",
       "Cancelled",
       "Completed",
       "Completed On Time",
@@ -60,9 +60,9 @@ const options: ApexOptions = {
     fontWeight: 500,
     fontSize: '14px',
 
-    markers: {
-      radius: 99,
-    },
+    // markers: {
+    //   radius: 99,
+    // },
   },
   fill: {
     opacity: 1,
